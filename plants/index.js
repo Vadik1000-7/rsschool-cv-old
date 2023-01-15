5847246d83d0e56d3cefefee3276a3441bfc0d99
Vadik1000-7 <petushok.v@mail.ru>
2023-01-15 19:32:39 +0300

    init: js structure

--- plants/index.js
+++ plants/index.js
@@ -1,4 +1,19 @@
-console.log('1. Верстка семантическая, читабельная \n 2. Все условия задания выполнены\n');
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let body = document.querySelector('.body');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+body.addEventListener('click', e => {
+	if(e.target.classList.contains('body')){
+		menuBtn.classList.toggle('active');
+		menu.classList.toggle('active');
+		body.classList.toggle('close');
+	}
+})
+
 
 
 
