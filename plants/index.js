1362bd874ce923932670bc5cc707827dfe7c0443
Vadik1000-7 <petushok.v@mail.ru>
2023-01-17 22:28:24 +0300

    fix: js

--- plants/index.js
+++ plants/index.js
@@ -1,15 +1,42 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
-let body = document.querySelector('.body');
+
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
-	body.classList.toggle('lock');
+
 })
 
 	
 
+const burgerMenu = () => {
 
+	let menu = document.querySelector('.menu-btn');
+	let menuNav = document.querySelector('.menu');
+	let body = document.querySelector('body');
+
+	body.addEventListener('click', (event) => {
+
+		let target = event.target;
+
+		if (target.closest('.menu-btn')) {
+			menu.classList.add('burger-open');
+			menuNav.style.display = 'block';
+		} else if (target.classList.contains('.menu-btn')) {
+			menu.classList.remove('burger-open');
+			menuNav.style.display = 'close';
+		} else if (target.tagName !== 'DIV') {
+			menu.classList.remove('burger-open');
+			menuNav.style.display = 'none';
+		} else {
+			return;
+		}
+		
+	});
+
+};
+
+burgerMenu();
 
 
