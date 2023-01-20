c989f8120246a9d93667f95b39f80a9651490f50
Vadik1000-7 <petushok.v@mail.ru>
2023-01-20 23:27:26 +0300

    init: burger-menu js

--- plants/index.js
+++ plants/index.js
@@ -1,42 +1,41 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
 
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
 
-})
+// Выполняем действия при клике ..
+function hambHandler(e) {
+e.preventDefault();
+// Переключаем стили элементов при клике
+popup.classList.toggle("open");
+hamb.classList.toggle("active");
+body.classList.toggle("noscroll");
+renderPopup();
+}
 
-	
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+popup.appendChild(menu);
+}
 
-const burgerMenu = () => {
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
 
-	let menu = document.querySelector('.menu-btn');
-	let menuNav = document.querySelector('.menu');
-	let body = document.querySelector('body');
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+link.addEventListener("click", closeOnClick);
+});
 
-	body.addEventListener('click', (event) => {
-
-		let target = event.target;
-
-		if (target.closest('.menu-btn')) {
-			menu.classList.add('burger-open');
-			menuNav.style.display = 'block';
-		} else if (target.classList.contains('.menu-btn')) {
-			menu.classList.remove('burger-open');
-			menuNav.style.display = 'close';
-		} else if (target.tagName !== 'DIV') {
-			menu.classList.remove('burger-open');
-			menuNav.style.display = 'none';
-		} else {
-			return;
-		}
-		
-	});
-
-};
-
-burgerMenu();
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+popup.classList.remove("open");
+hamb.classList.remove("active");
+body.classList.remove("noscroll");
+}
 
 
